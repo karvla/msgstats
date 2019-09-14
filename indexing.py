0403e3e2c5e23c19b9db94b387a98a677d5707a4
Arvid Larsson <arvla626@student.liu.se>
2019-09-14 10:02:02 +0200

    Improve indexing

--- indexing.py
+++ indexing.py
@@ -6,7 +6,7 @@ import regex as re
 inbox_path = 'messages/inbox/'
 stopwords = set((get_stop_words('en'))).union(set(get_stop_words('sv'))) # TODO: Make not hardcoded
 
-def _words(text): return list(re.finditer(r'\p{L}+', text.lower()))
+def _words(text): return list(re.findall(r'\p{L}+', text.lower()))
 
 def _decode(string): return string.encode('latin_1').decode('utf-8')
 
@@ -19,24 +19,6 @@ def _add_word_count(index, word):
         index[word] = 1
     return index
 
-def word_count(name):
-    """ word --> count"""
-    index = dict()
-    for dir_name, subdirs, thread in os.walk(inbox_path):
-        for fname in thread:
-            path = '/'.join([dir_name, fname])
-            if not re.findall('.json', path): continue
-            with open(path, encoding='utf-8') as f:
-                data = json.load(f)
-                if 'messages' not in data: continue
-                for msg in data['messages']:
-                    sender_name = _decode(msg['sender_name'])
-                    if sender_name == name and msg['type'] == 'Generic' and 'content' in msg:
-                        content = _decode(msg['content'])
-                        [_add_word_count(index, word.group()) for word in _words(content.lower())]
-    return index
-            
-
 def people_word_count():
     """ person -->  word --> nr"""
     index = dict()
@@ -48,12 +30,14 @@ def people_word_count():
                 data = json.load(f)
                 if 'messages' not in data: continue
                 for msg in data['messages']:
-                    if 'sender_name' in msg:
-                        name = _decode(msg['sender_name'])
+                    if not 'content' in msg: continue
+                    content = _decode(msg['content'])
+                    name = _decode(msg['sender_name'])
+                    for  word in _words(content):
                         if name in index:
-                            continue 
+                            _add_word_count(index[name], word) 
                         else:
-                            index[name] = word_count(name)
+                            index[name] = _add_word_count(dict(), word)
     return index 
 
 def word_people_count(pwc):
